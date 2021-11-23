f60a39b4ec856e80b05153ab17653683edd683ae
Felipe Godinez <ucdavis@Felipes-MacBook-Pro.local>
2021-11-22 18:28:00 -0800

    updated main

--- system.py
+++ system.py
@@ -6,15 +6,19 @@ class System():
     def __init__(self):
         self.sync = Sync('192.168.1.100')
         backend_ips = ['192.168.1.101', '192.168.1.102', '192.168.2.103', '192.168.2.104']
+        #backend_ips = ['192.168.2.104']
         self.backend = [Backend(a) for a in backend_ips]
+        self._stack = None
 
     def __enter__(self):
+        self.sync.set_network_led(clear = False)
         with ExitStack() as stack:
             [stack.enter_context(b) for b in self.backend]
             self._stack = stack.pop_all()
         return self
     
     def __exit__(self, *context):
+        self.sync.set_network_led(clear = True)
         if self._stack is not None:
             self._stack.__exit__(self, *context)
 
